74823dd2da8398861666d09f7610998ea9228d6e
AceCapitalEnterprise <acecapitalent@gmail.com>
2026-04-09 11:31:15 +0530

    Add files via upload

---
+++ Admin/Admin/css/myscript.js
@@ -0,0 +1,32 @@
+
+// $(document).ready(function () {
+
+//     $(".forgetopen").click(function () {
+//         if ($("#forget").is(':visible')) {
+//             $('#forget').hide();
+//             $('#login').show();
+//         } else {
+//             $('#login').hide();
+//             $('#forget').show();
+//         }
+//     })
+
+//     $("#fm").select(function()
+//     {
+//         let x = $("#fm").select().val()
+//         console.log(x)
+//    })
+   
+// });
+
+// var loadFile = function (event)
+// {
+//     var image = document.getElementById("output");
+//     image.src = URL.createObjectURL(event.target.files[0]);
+// };
+
+// let formbehaviours = (event) =>
+// {
+//     event.preventDefault();
+//     console.log('hello me dde3es,e')
+// }
